3413d7318535b1861a7353a0b06143b88e647650
YimKeul <43426556+YimKeul@users.noreply.github.com>
2022-01-04 05:06:45 +0900

    what the hell..

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,4 +1,10 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, {
+  useEffect,
+  useState,
+  useCallback,
+  useMemo,
+  useRef,
+} from "react";
 import {
   View,
   Text,
@@ -16,13 +22,12 @@ import {
 import { AntDesign } from "@expo/vector-icons";
 import { Entypo } from "@expo/vector-icons";
 import Home_card from "../components/Home_card";
-import axios from "axios"
-import {firebase_db} from '../firebaseConfig'
-import * as Application from 'expo-application';
+import axios from "axios";
+import { firebase_db } from "../firebaseConfig";
+import * as Application from "expo-application";
 import * as Location from "expo-location";
 
-const isIOS = Platform.OS === 'ios';
-
+const isIOS = Platform.OS === "ios";
 
 const wait = (timeout) => {
   return new Promise((resolve) => setTimeout(resolve, timeout));
@@ -37,24 +42,56 @@ const Home = ({ navigation, route }) => {
 
   const [refreshing, setRefreshing] = useState(false); //새로고침용
 
-  const [state,setState ] = useState([]); //firebase에서 데이터 저장
-  
-  const [weather, setWeather] = useState({temp: 0,condition: ""}); //날씨 저장
+  const [state, setState] = useState([]); //firebase에서 데이터 저장
 
-  const [ready,setReady] = useState(true)//대기
+  const [weather, setWeather] = useState({ temp: 999, condition: "" }); //날씨 처음 저장
+
+  const [ready, setReady] = useState(false); //대기
 
   useEffect(() => {
-    getLocation()
-    firebase_db.ref('/tip').once('value').then((snapshot) => {
-      console.log("파이어베이스에서 데이터 가져왔습니다!!")
-      let tip = snapshot.val();
-      setState(tip) 
-    });
-    console.log(weather)
-    setReady(false)
-    
-  }, []);
+    // firebase_db
+    //   .ref("/tip")
+    //   .once("value")
+    //   .then((snapshot) => {
+    //     console.log("파이어베이스에서 데이터 가져왔습니다!!");
+    //     let tip = snapshot.val();
+    //     setState(tip);
+    //     getLocation();
+    //     setReady(true);
 
+    //   });
+    // getLocation();
+    test();
+    // console.log(state.length)
+  }, [weather.condition]);
+
+  const [cate,setCate] = useState([])
+
+  const test = () => {
+    firebase_db
+      .ref("/tip")
+      .once("value")
+      .then((snapshot) => {
+        console.log("파이어베이스에서 데이터 가져왔습니다!!");
+        let tip = snapshot.val();
+        getLocation();
+        setState(tip);
+      });
+      console.log(state)
+    if (weather.condition == "구름") {
+      setCate(
+        state.filter((d) => {
+          return d.weather == "구름";
+        })
+      );
+    }else     if (weather.condition == "맑음") {
+      setCate(
+        state.filter((d) => {
+          return d.weather == "맑음";
+        })
+      );
+    }
+  };
 
   //날씨 정보 가져오기
   const getLocation = async () => {
@@ -65,81 +102,68 @@ const Home = ({ navigation, route }) => {
       const locationData = await Location.getCurrentPositionAsync();
       const latitude = locationData.coords.latitude;
       const longitude = locationData.coords.longitude;
-      const API_KEY = "62a3e1c15a974cb7212dd251dddb4fa7";
+      // const API_KEY = "62a3e1c15a974cb7212dd251dddb4fa7";
+      const API_KEY = "cfc258c75e1da2149c33daffd07a911d";
       const result = await axios.get(
-        `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`
       );
 
-      const weather = result.data.current.weather[0].main;
-      const temp = result.data.current.temp;
-     
-      if(weather ==='Drizzle' || weather === 'Rain' || weather ==='Thunderstorm'){
-        setWeather({ temp,  condition:'비'});
-      }else if (weather === 'Snow'){
-        setWeather({ temp,  condition:'눈'});
-      }else if(weather === 'Clear'){
-        setWeather({ temp,  condition:'맑음'});
-      }else if(weather === 'Clouds'){
-        setWeather({ temp, condition:'구름'});
-      }else{
-        setWeather({ temp,  condition:'알수없음'});
+      const _weather = result.data.weather[0].main;
+      const temp = result.data.main.temp;
+
+      if (
+        _weather === "Drizzle" ||
+        _weather === "Rain" ||
+        _weather === "Thunderstorm"
+      ) {
+        setWeather({ temp, condition: "비" });
+      } else if (_weather === "Snow") {
+        setWeather({ temp, condition: "눈" });
+      } else if (_weather === "Clear") {
+        setWeather({ temp, condition: "맑음" });
+      } else if (_weather === "Clouds") {
+        setWeather({ temp, condition: "구름" });
+      } else {
+        setWeather({ temp, condition: "알수없음" });
       }
-  
+
+      console.log(weather.condition + " " + weather.temp);
     } catch (error) {
-      //혹시나 위치를 못가져올 경우를 대비해서, 안내를 준비합니다
-      Alert.alert("위치를 찾을 수가 없습니다.", "앱을 껏다 켜볼까요?");
-      getLocation()
-      // console.log(error)
+      // Alert.alert("위치를 찾을 수가 없습니다.", "앱을 껏다 켜볼까요?")
+      console.log(error);
     }
   };
-  
-  // const getLocation = async () => {
-  //   //수많은 로직중에 에러가 발생하면
-  //   //해당 에러를 포착하여 로직을 멈추고,에러를 해결하기 위한 catch 영역 로직이 실행
-  //   try {
-  //     //자바스크립트 함수의 실행순서를 고정하기 위해 쓰는 async,await
-  //     await Location.requestForegroundPermissionsAsync();
-  //     const locationData= await Location.getCurrentPositionAsync();
-  //     console.log(locationData)
-
-  //   } catch (error) {
-  //     //혹시나 위치를 못가져올 경우를 대비해서, 안내를 준비합니다
-  //     console.log(error)
-  //     Alert.alert("위치를 찾을 수가 없습니다.", "앱을 껏다 켜볼까요?");
-  //   }
-  // }
-
 
   //새로고침
   const onRefresh = useCallback(() => {
     setRefreshing(true);
-    wait(2000).then(() => setRefreshing(false));
+    wait(2000).then(() => getLocation(), setRefreshing(false));
   }, []);
 
-
   //좋아요 함수
-  const I_LIKE = async() => {
-    
-    let userUniqueId
-      if(isIOS){
-          let iosID = await Application.getIosIdForVendorAsync()
-          userUniqueId = iosID
-      }else{
-          userUniqueId = await Application.androidId
-      }
+  const I_LIKE = async () => {
+    let userUniqueId;
+    if (isIOS) {
+      let iosID = await Application.getIosIdForVendorAsync();
+      userUniqueId = iosID;
+    } else {
+      userUniqueId = await Application.androidId;
+    }
 
-      console.log(userUniqueId)
-      //tip.idx 이부분에서 tip은 useState로 저장을 해야하는데 저장해야하는 데이터는
-      //날씨 + 기온 받아서 랜덤으로 뽑은 데이터를 저장한 훅이다.
-      //이건 로직 짜야함
-      firebase_db.ref('/like/'+userUniqueId+'/'+ state[2].idx).set(state[2],function(error){
-        console.log(error)
-        console.log(state[2])
-        Alert.alert("홈화면에서 저장!")
-    });
+    console.log(userUniqueId);
+    //tip.idx 이부분에서 tip은 useState로 저장을 해야하는데 저장해야하는 데이터는
+    //날씨 + 기온 받아서 랜덤으로 뽑은 데이터를 저장한 훅이다.
+    //이건 로직 짜야함
+    firebase_db
+      .ref("/like/" + userUniqueId + "/" + state[2].idx)
+      .set(state[2], function (error) {
+        console.log(error);
+        console.log(state[2]);
+        Alert.alert("홈화면에서 저장!");
+      });
   };
 
-  return ready? <View style ={{backgroundColor: "rgb(224,212,191)"}}></View> : (
+  return (
     <SafeAreaView style={{ backgroundColor: "rgb(224,212,191)", flex: 1 }}>
       <ScrollView
         refreshControl={
@@ -159,8 +183,10 @@ const Home = ({ navigation, route }) => {
           <View id="recommand_menu_card2" style={styles.recoomand_menu_card2}>
             <View id="image_title_space" style={styles.image_title_space}>
               <Text id="image_title" style={styles.image_title}>
-                Title
+                {/* Title */}
+                {cate.length}
               </Text>
+
             </View>
             <View
               id="image_space"
@@ -180,7 +206,9 @@ const Home = ({ navigation, route }) => {
           <TouchableOpacity
             id="recipe_btn"
             style={styles.recipe_btn}
-            onPress={() => navigation.navigate("Details",{idx : state[2].idx})}
+            onPress={() =>
+              navigation.navigate("Details", { idx: state[2].idx })
+            }
           >
             <AntDesign name="star" size={20} style={styles.icons} />
             <Text style={styles.recipe_text}>{` 레시피`}</Text>
@@ -205,14 +233,13 @@ const Home = ({ navigation, route }) => {
 
         <View>
           <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-            {state.map((content, i) => {
+            {cate.map((content, i) => {
               return (
-                <Home_card content={content} key={i} navigation={navigation}/>
+                <Home_card content={content} key={i} navigation={navigation} />
               );
             })}
           </ScrollView>
         </View>
-
 
         {/*이중선*/}
         <View
